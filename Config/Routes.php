43cd7ce0d81752d1398e2b732ca61d72b97575a0
Timothy Wood <codearachnid@gmail.com>
2020-05-19 16:38:42 -0400

    cleaned up restful verbs on routes

--- Config/Routes.php
+++ Config/Routes.php
@@ -1,23 +1,34 @@
 <?php namespace Config;
 
 $routes->group('auth', ['namespace' => 'AtomicAuth\Controllers'], function ($routes) {
-  $routes->get('/', 'Auth::index');
+
+  // ... generic auth routes
+  $routes->add('/', 'Auth::index');
   $routes->add('login', 'Auth::login');
-  $routes->get('logout', 'Auth::logout');
+  $routes->add('logout', 'Auth::logout');
   $routes->add('forgot', 'Auth::forgot_password');
-  $routes->add('create_user', 'Auth::create_user');
-  $routes->add('edit_user/(:num)', 'Auth::edit_user/$1');
-  $routes->add('create_group', 'Auth::create_group');
-  $routes->get('activate/(:num)', 'Auth::activate/$1');
-  $routes->get('activate/(:num)/(:hash)', 'Auth::activate/$1/$2');
-  $routes->add('deactivate/(:num)', 'Auth::deactivate/$1');
+  // ...
+
+  // ... user centric
+  $routes->put('create', 'User::create');
+  $routes->patch('edit/(:hash)', 'User::edit/$1');
+  $routes->add('activate/(:hash)', 'User::activate/$1');
+  // $routes->get('activate/(:num)/(:hash)', 'Auth::activate/$1/$2');
+  $routes->delete('suspend/(:hash)', 'User::suspend/$1');
+  // ...
+
+  // ... group/roles
+  $routes->add('group/create', 'Group::create');
+  $routes->patch('group/edit/(:hash)', 'Group::edit/$1');
+  $routes->delete('suspend/(:hash)', 'Group::suspend/$1');
+  // ...
 
   // ... be careful leaving these in if you aren't sure what they do
   $routes->add('admin', 'Admin::go_away');
   $routes->add('admin/generate', 'Admin::salt');
   $routes->add('admin/generate/(:num)', 'Admin::salt/$1');
-  $routes->add('admin/install/(:hash)', 'Admin::install/$1');
-  $routes->add('admin/uninstall/(:hash)', 'Admin::uninstall/$1');
+  $routes->add('admin/install?(:hash)', 'Admin::install/$1');
+  $routes->add('admin/uninstall?(:hash)', 'Admin::uninstall/$1');
   // ...
-  
+
 });
