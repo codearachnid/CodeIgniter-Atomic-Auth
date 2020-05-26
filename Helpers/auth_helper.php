4cbc3b14c5e26acae699a42a8119b7c1de1b6d7f
Timothy Wood <codearachnid@gmail.com>
2020-05-26 09:10:03 -0400

    added userCan & userInGroup checks

--- Helpers/auth_helper.php
+++ Helpers/auth_helper.php
@@ -13,40 +13,28 @@ if(! function_exists('loggedIn'))
 		return $atomicAuth->loggedIn();
 	}
 }
-//
-// if (! function_exists('isAdmin'))
-// {
-// 	/**
-// 	 * Searches an array through dot syntax. Supports
-// 	 * wildcard searches, like foo.*.bar
-// 	 *
-// 	 * @param string $index
-// 	 * @param array  $array
-// 	 *
-// 	 * @return mixed|null
-// 	 */
-// 	// TODO determine if we should allow config/model to pass through
-// 	// function isAdmin($userOrId, \Config\App $altConfig = null) : bool
-// 	function isAdmin($userOrId = null) : bool
-// 	{
-// 		$isAdmin = false;
-// 		// use alternate config if provided, else default one
-// 		// $config = $altConfig ?? config(\Config\App::class);
-// 		$config = config('AtomicAuth');
-// 		$this->atomicAuthModel = new \AtomicAuth\Models\AtomicAuthModel();
-//
-// 			// convert segment array to string
-// 			if (is_array($uri))
-// 			{
-// 				$uri = implode('/', $uri);
-// 			}
-//
-//
-//
-//
-// 		return (bool) $isAdmin;
-// 	}
-// }
+if(! function_exists('userCan'))
+{
+	function userCan( string $key = null ) : bool
+	{
+		if( is_null($key) )
+		{
+			return false;
+		}
+		$atomicAuth = new \AtomicAuth\Libraries\AtomicAuth();
+		return $atomicAuth->userCan( $key );
+	}
+}
+if(! function_exists('userInGroup'))
+{
+	function userInGroup( string $key = null ) : bool
+	{
+		if( is_null($key) )
+		{
+			return false;
+		}
+	}
+}
 if(! function_exists('isAdmin'))
 {
 	function isAdmin() : bool
@@ -65,7 +53,7 @@ if(! function_exists('isDefault'))
 }
 if(! function_exists('getUserProfile'))
 {
-	function getUserProfile() : bool
+	function getUserProfile()
 	{
 		$atomicAuth = new \AtomicAuth\Libraries\AtomicAuth();
 		return $atomicAuth->getUserProfile();
@@ -73,7 +61,7 @@ if(! function_exists('getUserProfile'))
 }
 if(! function_exists('getUserId'))
 {
-	function getUserId() : bool
+	function getUserId() : int
 	{
 		$atomicAuth = new \AtomicAuth\Libraries\AtomicAuth();
 		return $atomicAuth->getUserProfile('user_id');
